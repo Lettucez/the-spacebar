2334a66ec58283eecfb5e8590ede339bd71495f8
Christopher Leo <cleo@wvuf.org>
2019-02-06 18:28:59 -0500

    add twig template

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,10 +9,11 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * Shows the home page.
@@ -24,5 +25,24 @@ class ArticleController
     public function homepage()
     {
         return new Response("Hello Bros");
+    }
+
+    /**
+     * Shows an article.
+     *
+     * @param string $article Contains the url text after /news/.
+     *
+     * @return Response
+     *
+     * @Route("/news/{article}")
+     */
+    public function show($article)
+    {
+        return $this->render('article/show.html.twig', [
+            'article' => $article,
+            'items' => [
+                'item1', 'item2', 'item3'
+            ]
+        ]);
     }
 }
